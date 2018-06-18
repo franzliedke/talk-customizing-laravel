3e2ad6dd7fd5d64b7355b18d830a473f7d140712
Franz Liedke <franz@develophp.org>
2018-06-18 21:25:45 +0200

    Create a skeleton for our ReCaptcha package

---
+++ recaptcha/packages/recaptcha/src/RecaptchaServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace LaraLive\Recaptcha;
+
+use Illuminate\Support\ServiceProvider;
+
+class RecaptchaServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        //
+    }
+
+    public function boot()
+    {
+        //
+    }
+}
