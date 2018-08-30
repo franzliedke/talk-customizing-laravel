435b302eab0144c5132930d6e661a36212dab310
Franz Liedke <franz@develophp.org>
2018-08-30 17:34:45 +0200

    [STEP 7] Extend Validator with "human" rule
    Again, we want to avoid forcing the developer to have internal
    knowledge about our package.
    
    By using the validation component's extension point, we can add
    a string-based rule - now our ReCaptcha validation fits in
    perfectly with the other validation rules! We have to use
    `extendImplicit` instead of `extend` here - otherwise we would
    not be informed about missing values. We need to, though:
    missing values would mean a failure for ReCaptcha (such as
    someone trying to manually send a request without the ReCaptcha
    response field).
    
    We have seen this pattern before: many Laravel components offer
    "macro" methods for extension purposes, typically with a name
    and a closure.

--- recaptcha/packages/recaptcha/src/RecaptchaServiceProvider.php
+++ recaptcha/packages/recaptcha/src/RecaptchaServiceProvider.php
@@ -3,6 +3,7 @@
 namespace LaraLive\Recaptcha;
 
 use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\ServiceProvider;
 use ReCaptcha\ReCaptcha;
 
@@ -30,6 +31,17 @@ class RecaptchaServiceProvider extends ServiceProvider
 <div class="g-recaptcha" data-sitekey="$key"></div>
 HTML;
             }
+        );
+
+        Validator::extendImplicit(
+            'human',
+            function ($attribute, $value) {
+                $captcha = $this->app->make(ReCaptcha::class);
+                $request = $this->app->make('request');
+
+                return $captcha->verify($value, $request->getClientIp())->isSuccess();
+            },
+            'Please confirm you are human.'
         );
     }
 }
